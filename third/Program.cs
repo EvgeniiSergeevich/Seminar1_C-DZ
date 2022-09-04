e8018fee31148af70e392b33fca7a37120d85051
Evgeniy <zheinirk38@gmail.com>
2022-09-04 16:27:32 +0800

    Completed third task

---
+++ third/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter the integer number: ");
+try {
+    int i = Convert.ToInt32(Console.ReadLine());
+    if(i%2 == 0) {
+        Console.Write("The number " + i + " is even");
+    } else {
+        Console.Write("The number " + i + " is not even");
+    }
+} catch {
+    Console.WriteLine("The number is not integer!");
+}
