b16a337d58d9a872c599d0425d7ec47d22faf87c
Evgeniy <zheinirk38@gmail.com>
2022-09-04 16:35:28 +0800

    Completed fourth task

---
+++ fourth/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter the integer number: ");
+try{
+    int x = Convert.ToInt32(Console.ReadLine());
+    int i = 1;
+    while (i <= x) {
+        if(i % 2 == 0) {
+            Console.Write(i + ", ");
+        }
+        i++;
+    }
+} catch {
+    Console.WriteLine("The number is not integer!!!1!");
+}
