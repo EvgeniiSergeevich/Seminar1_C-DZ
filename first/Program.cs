be0bc24e4f614c7e7e463e1858591afda9555f8d
Evgeniy <zheinirk38@gmail.com>
2022-09-04 16:07:09 +0800

    Completed the 1 task

---
+++ first/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter two intrger numbers");
+try {
+    int x = Convert.ToInt32(Console.ReadLine());
+    int y = Convert.ToInt32(Console.ReadLine());
+    if(x < y) {
+        Console.Write("Number " + x + " is less than number " + y);
+    } else if(x > y) {
+        Console.Write("Number " + x + " is greater than number " + y);
+    }
+    else {
+        Console.Write("The numbers are equal");
+    }
+
+} catch {
+    Console.WriteLine("Enter correct numbers");
+}    
