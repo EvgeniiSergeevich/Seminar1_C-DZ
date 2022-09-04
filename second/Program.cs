bfff1ca6ec4961be0150502ad3eca9e36cddaa0f
Evgeniy <zheinirk38@gmail.com>
2022-09-04 16:20:09 +0800

    Completed the second task

---
+++ second/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter three integer numbers: ");
+try {
+    int i = Convert.ToInt32(Console.ReadLine());
+    int j = Convert.ToInt32(Console.ReadLine());
+    int k = Convert.ToInt32(Console.ReadLine());
+    int max = i;
+
+    if(max < j) {
+        max = j;
+    }
+    if (max < k) {
+        max = k;
+    }
+    Console.WriteLine("Max number = " + max);
+} catch {
+    Console.WriteLine("Enter numbers again!");
+}
